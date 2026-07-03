04b9bd824a5b8d967f3c62e8915321a7ca5ee7a2
mohamad407 <asihfibrahim600@gmail.com>
2026-07-03 21:18:54 +0530

    Create admin model with schema for Mongoose

---
+++ backend/models/admin.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    role: {
+        type: String,
+        default: 'admin'
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Admin', adminSchema);

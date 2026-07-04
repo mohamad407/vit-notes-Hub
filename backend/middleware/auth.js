81962065b9f7491611c61f0b5b7698d61ee11e8f
mohamad407 <asihfibrahim600@gmail.com>
2026-07-04 10:53:46 +0530

    Fix indentation in auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,6 @@
 const { getAuth } = require('../config/firebase');
-   const User = require('../models/user');
+const User = require('../models/user');
 const Admin = require('../models/admin');
-
 // Verify Firebase token and attach user
 const verifyToken = async (req, res, next) => {
     try {
